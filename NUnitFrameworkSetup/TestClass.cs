7c2d84c04f0a27d7414d6c5f4c071dfb0402aa07
Oladeni1 <olatundeoladeni@yahoo.com>
2020-01-29 04:31:30 +0000

    checkIn

---
+++ NUnitFrameworkSetup/TestClass.cs
@@ -0,0 +1,35 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace NUnitFrameworkSetup
+{
+    [TestFixture]
+    public class TestClass
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [SetUp]
+        public void LunchBrowser()
+        {
+            //Open website
+            driver.Navigate().GoToUrl("https://www.udemy.com/");
+        }
+        [Test]
+        public void RunTest()
+        {
+          //Write all your tests 
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            //Close browser session
+            driver.Close();
+        }
+    }
+}
